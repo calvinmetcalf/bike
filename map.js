46e6b7cf3fdb7136b2ccceef7b2069095c44a51c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-10-31 15:05:21 -0400

    updated map

--- map.js
+++ map.js
@@ -1,127 +1,42 @@
-var m;
-var tid = 3831965;
-var gwid = 3847334;
-var geocoder = new google.maps.Geocoder();
-var zoom = 8;
-var center = new google.maps.LatLng(42.04113400940814,-71.795654296875);
-var marker;
-var mainLayer,greenWay;
+var m = new L.Map("map", {
+    center: new L.LatLng(42.2, -71),
+	zoom: 8,
+	attributionControl: true
+});
+new L.Hash(m);
+var mapQuestAttr = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; ';
+var osmMapAttr = '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+  var osmDataAttr = 'Map data ' + osmMapAttr;
+var opt = {
+    url: 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg',
+    options: {attribution:mapQuestAttr + osmDataAttr, subdomains:'1234'}
+  };
+var mq=L.tileLayer(opt.url,opt.options);
+mq.addTo(m);
+var bike=L.geoJson('',{style:style,onEachFeature:popUp}).addTo(m);
+$.get("bike.geojson",function(d){bike.addData(d);},"JSON");
 
-$(function() {
-        $( "#tabs" ).tabs({
-        	collapsible: true,
-            selected: -1
-		});
-        $( "input:submit,input:reset" ).button();
-        $('input, textarea').placeholder();
-        fusion();
-        popLists();
-	});
-function fusion() {
-    
-  m = new google.maps.Map(document.getElementById('map'), {
-      center: center,
-      zoom: zoom,
-      mapTypeId: 'roadmap'
-    });
-    
- /*greenWay =   new google.maps.FusionTablesLayer({
-  query: {
-    select: 'geometry',
-    from: gwid,
-    where: "'Preferred' ='Y'"
-  },
-  styles: [{
-    polylineOptions: {
-      strokeColor: "#F6FF00",
-      strokeOpacity: 0.6,
-      strokeWeight:6
+function style (f) {
+    var sopt={opacity:1,weight:3}
+    switch (f.properties.FacilityType) {
+            case "Bike lane": sopt.color = "#ff0000"; return sopt;
+            case "Shared use path":   sopt.color = "#00ff00"; return sopt;
+            case "To be determined (retired type - hold for future use, use for unknown faciliy)":   sopt.color = "#ffffff"; return sopt;
+            case "Marked shared lane": sopt.color = "#F2A90A"; return sopt;
+            case "Sign-posted on-road bike route (with no other accommodation on the road surface)": sopt.color = "#919191"; return sopt;
+            case "Bicycle/Pedestrian priority roadway": sopt.color = "#FFFB00"; return sopt;
+            case "Usable bike shoulder (4-5 foot min./moderate volume/speed road, locally identified as a bike facility)": sopt.color = "#919191"; sopt.dashArray="5, 5"; return sopt;
+            case "Hybrid (raod segment with different treatments in each direction of travel)": sopt.color = "#919191"; sopt.dashArray="5, 5, 1, 5"; return sopt;
+            case "Cycle track": sopt.color = "#EA00FF"; sopt.dashArray="5, 5, 1, 5"; return sopt;
+        }
+        return sopt;
+}
+function popUp(f,l){
+    var out = [];
+    if (f.properties){
+        for(key in f.properties){
+            out.push(key+": "+f.properties[key])
+        }
+        l.bindPopup(out.join("<br />"));
     }
-  }]
-});*/
-greenWay = new google.maps.FusionTablesLayer(gwid);
- mainLayer = new google.maps.FusionTablesLayer(tid);
- greenWay.setQuery("SELECT 'geometry' FROM " + gwid + " WHERE 'Preferred' ='Y'" );
-  mainLayer.setQuery("SELECT 'geometry' FROM " + tid + " WHERE 'Status' like 'Constructed (existing)'");
-  greenWay.setMap(m);
-  greenWay.setOptions({suppressInfoWindows:true});
-  mainLayer.setMap(m);
-  }
-
-function geocode() {
-     var address = document.getElementById("address").value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        m.setCenter(results[0].geometry.location);
-        m.setZoom(14);
-     marker = new google.maps.Marker({
-            map: m, 
-            position: results[0].geometry.location
-        });
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-}
-
-function resetgeo() {
-    
-    m.setCenter(center);
-    m.setZoom(zoom);
-marker.setMap(null);
-}
-
-
-    
-    google.load('visualization', '1', {});
-    
-function popLists(){    
-    MakePopList('NewType',getFacTypeData);
-   MakePopList('Status',getStatusData);
-  
-    }
-
-function MakePopList(columnName,callfunc){
- var queryText = encodeURIComponent("SELECT " +columnName + ", COUNT() FROM " + tid + " GROUP BY " +columnName);
-    var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + queryText);
-    query.send(callfunc);
-	}
-    
-var getFacTypeData = MakeData("facType"," 'NewType' like '");
-var getStatusData = MakeData("facStatus"," 'Status' like '");
-
-function MakeData(selectID,querryText){
-
-function getData(response) {
-  // Get the number of rows
-var numRows = response.getDataTable().getNumberOfRows();
-  
-  // Add options to the select menu based on the results
- var typeSelect = document.getElementById(selectID);  
-  for(i = 0; i < numRows; i++) {
-      var ftData = response.getDataTable().getValue(i, 0);
-     
-    
-     
-      var newoption = document.createElement('option');
-      newoption.setAttribute('value',querryText + ftData + "'");
-    newoption.innerHTML = ftData;
-     if (ftData == 'Constructed (existing)')
-     { newoption.setAttribute("selected","selected");}
-    typeSelect.appendChild(newoption);
-  }  
-}
-return getData;
-}    
-    
-    
-
-function changeMap() {
-   var facType = document.getElementById('facType').value.replace("'", "\\'");
-   var statusType = document.getElementById('facStatus').value.replace("'", "\\'");
-   var andz = " and ";
- if(facType === "" ||statusType === "")
- {andz = "";}
-  mainLayer.setQuery("SELECT 'geometry' FROM " + tid + " WHERE " + facType + andz + statusType);
- 
 }
