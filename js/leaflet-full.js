994bdf4411c993b26e8b3dbea8761b0da94a1a2d
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-03-28 17:40:49 -0400

    hash update

--- js/leaflet-full.js
+++ js/leaflet-full.js
@@ -8754,9 +8754,11 @@ L.control.layers.provided = function (baseLayers, overlays, options) {
       }
       if (this.options.lc && !this.options.formatBase) {
         this.options.formatBase = [
-          /[\sA-Z]/g, function(match) {
+          /[(?:\s|\:)A-Z]/g, function(match) {
             if (match.match(/\s/)) {
               return "_ ";
+            } else if (match.match(/\:/)) {
+            	return ""
             }
             if (match.match(/[A-Z]/)) {
               return match.toLowerCase();
