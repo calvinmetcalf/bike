634a6c90964cfff5bbf04525f0d487ee8136feec
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-03-28 15:24:33 -0400

    more

--- js/leaflet-full.js
+++ js/leaflet-full.js
@@ -8704,7 +8704,7 @@ L.Control.Layers.Provided = L.Control.Layers.extend({
 								var out = {},
 								len = base.length,
 								i=0;
-								while(i<len){if(base[i].replace){
+								while(i<len){if(typeof base[i]===){
 										if (i === 0) {
 												first = L.tileLayer.provider(base[0]);
 												out[base[i].replace(/\./g,": ").replace(/([a-z])([A-Z])/g,"$1 $2")] = first;
